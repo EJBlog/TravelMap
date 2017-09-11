bb8f7b9c950a5e4f94b94ec34b1a780726b6e1d9
Jake Stout <kyjstout@gmail.com>
2017-09-10 21:29:47 -0400

    Responsive homepage

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('styles/bootstrap-override.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('build/css'))
+});
